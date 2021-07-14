43140f17017f8f14994f95f2e44d0b84ae6fb777
Grant Ales <grant.ales@yahoo.com>
2021-07-13 21:36:50 -0600

    Added winning music

--- project_template/game/data/win_menu.py
+++ project_template/game/data/win_menu.py
@@ -11,6 +11,9 @@ Functions:  __init__()
 import arcade
 from arcade.gui import UIManager
 from data import constants
+import time
+
+MUSIC_VOLUME = 0.5
 
 class Win_Menu(arcade.View):
     """ This class creates the Win menu when a win condition is met.
@@ -35,12 +38,30 @@ class Win_Menu(arcade.View):
         super().__init__()
         self.game_view = game_view
 
+        # Music
+        self.music_list = []
+        self.current_song_index = 0
+        self.current_player = None
+        self.music = None
+
+    def play_song(self):
+
+        """ Play the song. """
+        # Stop what is currently playing.
+        if self.music:
+            self.music.stop(self.current_player)
+
+        self.music = arcade.Sound(self.music_list[self.current_song_index], streaming=True)
+        self.current_player = self.music.play(MUSIC_VOLUME)
+        time.sleep(0.3)
+
     def on_show(self):
         """ Sets the background color of the Win menu.
         
         Args:
             self (Win_Menu): an instance of Win_Menu.
-        """   
+        """
+        self.setup()
         arcade.set_background_color(arcade.color.BLACK)
 
     def on_draw(self):
@@ -50,7 +71,7 @@ class Win_Menu(arcade.View):
             self (Win_Menu): an instance of Win_Menu.
         """  
         arcade.start_render()
-
+        
         arcade.draw_text("You Won!!", constants.SCREEN_WIDTH/2, constants.SCREEN_HEIGHT/2+50,
                          arcade.color.WHITE, font_size=50, anchor_x="center")
 
@@ -68,6 +89,15 @@ class Win_Menu(arcade.View):
                          font_size=20,
                          anchor_x="center")
 
+    def setup(self):
+
+        # plays the winning song.
+        self.song1 = constants.assets_dir / "sounds" / "121 The Hall of Fame.mp3"
+        self.music_list = [self.song1]
+        self.current_song_index = 0
+        self.play_song()
+
+
     def on_key_press(self, key, _modifiers):
         """ Allows the user to end or restart the game on key press.
         
@@ -76,7 +106,9 @@ class Win_Menu(arcade.View):
             key (arcade.key): sets the keys to end or restart the game.
         """   
         if key == arcade.key.ESCAPE:   # end game
+            # self.music.stop(self.current_player)
             self.window.close()
         elif key == arcade.key.ENTER:  # restart game
+            self.music.stop(self.current_player)
             self.game_view.start_new_game()
             self.window.show_view(self.game_view)
